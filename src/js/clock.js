d82318411df5a1c50a466b07fddb927f8aa983e8
csw <woncs95@gmail.com>
2021-11-28 19:44:55 +0100

    changed clock.js setiings

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,8 +1,8 @@
 const clock = document.querySelector("#clock");
 const date = new Date();
-let hours = String(date.getHours).padStart(2,"0");
-let minutes = String(date.getMinutes).padStart(2,"0");
-let seconds = String(date.getSeconds).padStart(2,"0");
+let hours = String(date.getHours()).padStart(2,"0");
+let minutes = String(date.getMinutes()).padStart(2,"0");
+let seconds = String(date.getSeconds()).padStart(2,"0");
 
 function showTime(){
     clock.innerText = `${hours}:${minutes}:${seconds}`
