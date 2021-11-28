eaa9b19c3a1dd7d96c709c08b62efc2785cb069f
csw <woncs95@gmail.com>
2021-11-28 20:29:47 +0100

    quote.js, clock.js, background.js changed

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,11 +1,11 @@
 const clock = document.querySelector("#clock");
-const date = new Date();
-let hours = String(date.getHours()).padStart(2,"0");
-let minutes = String(date.getMinutes()).padStart(2,"0");
-let seconds = String(date.getSeconds()).padStart(2,"0");
 
 function showTime(){
+const date = new Date();
+const hours = String(date.getHours()).padStart(2,"0");
+const minutes = String(date.getMinutes()).padStart(2,"0");
+const seconds = String(date.getSeconds()).padStart(2,"0");
     clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 
-showTime();
+setInterval(showTime, 1000);
