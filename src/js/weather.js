840d93bcfd8bd1ad902f5f88105c1d0967fa34e7
csw <woncs95@gmail.com>
2021-11-28 22:56:54 +0100

    changed css

--- src/js/weather.js
+++ src/js/weather.js
@@ -3,7 +3,6 @@ const API_KEY = "8668cbefcb936cb698f16077ee08703b"
 function onGeoOK(position){
  const lat = position.coords.latitude;
  const lon = position.coords.longitude;
- 
  const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
  fetch(url)
  .then((response)=>response.json())
