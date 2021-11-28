bbdec3ffaa5fa98938991e9e9f5fb0fe6b3993d7
csw <woncs95@gmail.com>
2021-11-28 22:35:45 +0100

    changed css

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,6 @@
 const images = ["0.png","1.png","2.png","3.png","4.png","5.png", "6.png", "7.png", "8.png"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
-bgImage.src= `assets/img/${chosenImage}`;
-document.body.appendChild(bgImage);
+//const bgImage = document.createElement("img");
+document.body.style.backgroundImage = `url(assets/img/${chosenImage})`;
+
+//document.body.appendChild(bgImage);
