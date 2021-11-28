e73e2710dfed16c791a15ceec2db451a54f1ddf0
csw <woncs95@gmail.com>
2021-11-28 19:37:47 +0100

    initial commit

---
+++ src/js/clock.js
@@ -0,0 +1,11 @@
+const clock = document.querySelector("#clock");
+const date = new Date();
+let hours = String(date.getHours).padStart(2,"0");
+let minutes = String(date.getMinutes).padStart(2,"0");
+let seconds = String(date.getSeconds).padStart(2,"0");
+
+function showTime(){
+    clock.innerText = `${hours}:${minutes}:${seconds}`
+}
+
+showTime();
