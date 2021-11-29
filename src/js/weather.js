28b1f29faffbab12c47c55247b1f4a66bda64807
Chang Seup Wyon <woncs95@gmail.com>
2021-11-29 01:21:12 +0100

    key security and changed some feature

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,16 +1,20 @@
-const API_KEY = "8668cbefcb936cb698f16077ee08703b"
+// API_KEY = fetch("config/key.json")
+// .then(response => response.json()).catch((err)=>console.log(err));
+require('dotenv').config();
+console.log(process.env);
+
 
 function onGeoOK(position){
  const lat = position.coords.latitude;
  const lon = position.coords.longitude;
- const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+ const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
  fetch(url)
  .then((response)=>response.json())
  .then((data)=>{
      const weather = document.querySelector("#weather span:first-child");
      const city = document.querySelector("#weather span:last-child");
     city.innerText = data.name;
-weather.innerText = `${data.weather[0].main}/${data.main}`});
+weather.innerText = `${data.weather[0].main}`});
 }
 
 function onGeoError(){
